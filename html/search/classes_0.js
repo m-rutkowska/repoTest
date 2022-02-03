84cb1ed5ee7c0f1f35ad85ed8c9b6cbcf99080c8
m-rutkowska <bigfatluu@gmail.com>
2022-02-03 21:42:07 +0100

    new commit

---
+++ html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mysin_0',['MySin',['../class_my_sin.html',1,'']]]
+];
---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['repo161690_0',['repo161690',['../md__r_e_a_d_m_e.html',1,'']]]
+];
